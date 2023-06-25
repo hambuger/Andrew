a7fdff33b229a3d3dbe2e2d725a5726d36744592
hamburger <hanjbde@163.com>
2023-06-25 22:51:51 +0800

    图片识别工具

--- openai_util/function_call/__init__.py
+++ openai_util/function_call/__init__.py
@@ -1,3 +1,3 @@
 # 初始化时加载类
-import openai_util.function_call.register_function
-import openai_util.function_call.openaifunc_decorator
+from openai_util.function_call import funcation_invoke, openaifunc_decorator, register_function
+from image_util import image_recognition
