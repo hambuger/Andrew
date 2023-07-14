ab94cd8675d2653b473d85034356f6c80a87fcc6
hamburger <HanJBde@163.com>
2023-07-14 11:41:29 +0800

    fix

--- image_util/image_recognition.py
+++ image_util/image_recognition.py
@@ -19,7 +19,7 @@
 # print(describe_image(
 #     'https://images.unsplash.com/photo-1618843479313-40f8afb4b4d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
 #     'the brand of the car in the picture is '))
-
+import requests
 from PIL import Image
 from transformers import AutoProcessor, BlipForQuestionAnswering
 from translate import Translator
@@ -51,7 +51,10 @@ def answer_by_image_url_and_text(image: str, prompt: str):
         model = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-base")
     if not processor:
         processor = AutoProcessor.from_pretrained("Salesforce/blip-vqa-base")
-    image = Image.open(image)
+    if str.startswith(image, 'http'):
+        image = Image.open(requests.get(image, stream=True).raw)
+    else:
+        image = Image.open(image)
     inputs = processor(images=image, text=translator.translate(prompt), return_tensors="pt")
     return processor.decode(model.generate(**inputs, max_length=4000)[0], skip_special_tokens=True)
 
