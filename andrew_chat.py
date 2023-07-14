f93f0de5acec55a0fed2905c136686f69273b38e
hamburger <HanJBde@163.com>
2023-07-14 13:52:09 +0800

    命令行停止语音播放

--- andrew_chat.py
+++ andrew_chat.py
@@ -5,8 +5,12 @@ from config.global_logger import logger
 from paddlespeech.cli.asr.infer import ASRExecutor
 from openai_util.s_auto_gpt import run_conversation_v2, push_message
 from voice_util.kws.audio_kws import get_audio
-from voice_util.tts.voice_tts import text_2_audio
+from voice_util.tts.voice_tts import text_2_audio, stop_speak
 from database_util.redis.redis_client import api_key_manager
+from concurrent.futures import ThreadPoolExecutor
+
+executor = ThreadPoolExecutor(10)
+input_str = None
 
 # 创建ASR执行器
 asr = ASRExecutor()
@@ -19,17 +23,32 @@ audio_active = False
 file_path = 'audio.wav'
 # 记录上一次对话回应消息的ID
 parent_id = '0'
+
+
+def get_input():
+    global input_str
+    while True:
+        input_str = input()
+        if not input_str:
+            continue
+        if input_str.lower() == 'stop':
+            stop_speak()
+            input_str = None
+            continue
+
+
+executor.submit(get_input)
 while True:
     try:
         get_audio(audio_active, file_path, last_input_time)
         # 执行ASR并打印结果
-        result = asr(model='conformer_wenetspeech', audio_file=file_path, force_yes=True)
-        logger.info(f"识别语音：{result}")
+        audio_text = asr(model='conformer_wenetspeech', audio_file=file_path, force_yes=True)
+        logger.info(f"识别语音：{audio_text}")
         last_input_time = time.time()
         audio_active = True
-        if not result:
+        if not audio_text:
             continue
-        if '再见' in result or '再見' in result:
+        if '再见' in audio_text or '再見' in audio_text:
             logger.info("Bye!")
             audio_active = False
             parent_id = '0'
@@ -37,8 +56,10 @@ while True:
             if api_key_manager.get_key_value('AUDIO_KEY') == os.getenv('os_name'):
                 api_key_manager.delete_key('AUDIO_KEY')
             continue
-        push_message({"role": "user", "content": result})
-        (answer, msg_Id) = run_conversation_v2(result, parent_id)
+        if input_str:
+            audio_text = f"""{input_str} \n{audio_text}"""
+        push_message({"role": "user", "content": audio_text})
+        (answer, msg_Id) = run_conversation_v2(audio_text, parent_id)
         if msg_Id:
             parent_id = msg_Id
         if not answer:
--- voice_util/tts/voice_tts.py
+++ voice_util/tts/voice_tts.py
@@ -1,22 +1,14 @@
-import os
 import pyttsx3
-import subprocess
 
-engine = None
+engine = pyttsx3.init()
 
 
 def text_2_audio(text):
     # tts(am="fastspeech2_male", voc="pwgan_male", text=text, output=output, use_onnx=True)
-    os_name = os.getenv('os_name', 'windows')
-    if os_name == 'macos':
-        subprocess.run(["say", text])
-    else:
-        global engine
-        if not engine:
-            # 初始化语音引擎
-            engine = pyttsx3.init()
-            # 获取所有可用的语音
-            voices = engine.getProperty('voices')
-            engine.setProperty('voice', voices[0].id)
-        engine.say(text)
-        engine.runAndWait()
+    engine.setProperty('rate', 120)
+    engine.say(text)
+    engine.runAndWait()
+
+
+def stop_speak():
+    engine.stop()
