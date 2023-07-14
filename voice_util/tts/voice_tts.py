3a2de0e7d83695f454d8351beb5b66003f76cbf3
hamburger <hanjbde@163.com>
2023-07-14 22:15:22 +0800

    fix

--- voice_util/tts/voice_tts.py
+++ voice_util/tts/voice_tts.py
@@ -5,7 +5,7 @@ engine = pyttsx3.init()
 
 def text_2_audio(text):
     # tts(am="fastspeech2_male", voc="pwgan_male", text=text, output=output, use_onnx=True)
-    engine.setProperty('rate', 120)
+    # engine.setProperty('rate', 120)
     engine.say(text)
     engine.runAndWait()
 
