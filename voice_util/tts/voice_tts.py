4784a58e45d4b0a5076c1a8b6dd1ae72e30e1a1e
hamburger <hanjbde@163.com>
2023-07-19 00:23:14 +0800

    兼容Windows的异步stop语音播放

--- voice_util/tts/voice_tts.py
+++ voice_util/tts/voice_tts.py
@@ -1,14 +1,31 @@
+import os
 import pyttsx3
+import win32com.client
 
-engine = pyttsx3.init()
+engine = None
+os_name = os.getenv('os_name', 'windows')
+if os_name == 'windows':
+    engine = win32com.client.Dispatch('SAPI.SPVoice')
+elif os_name == 'macos':
+    engine = pyttsx3.init()
 
 
 def text_2_audio(text):
-    # tts(am="fastspeech2_male", voc="pwgan_male", text=text, output=output, use_onnx=True)
-    # engine.setProperty('rate', 120)
-    engine.say(text)
-    engine.runAndWait()
+    if os_name == 'windows':
+        engine.Speak(text, 19)
+        while engine.Status.RunningState != 1:
+            pass
+    elif os_name == 'macos':
+        engine.setProperty('rate', 120)
+        engine.say(text)
+        engine.runAndWait()
 
 
 def stop_speak():
-    engine.stop()
+    if os_name == 'windows':
+        if engine.Status.RunningState == 2:
+            engine.Speak('', 2)
+            while engine.Status.RunningState != 1:
+                pass  # 等待语音输出完全停止while engine.Status.RunningState != 1:
+    elif os_name == 'macos':
+        engine.stop()
