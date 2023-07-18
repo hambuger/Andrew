6398f1a8e6c14e9a91bd8450aff405ae46a91cac
hamburger <HanJBde@163.com>
2023-07-18 16:10:52 +0800

    下载文件

--- info_util/download.py
+++ info_util/download.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from openai_util.function_call.openaifunc_decorator import openai_func
 
@@ -9,6 +10,13 @@ def download_file(url: str, save_filename: str):
     :param url: the url of the file
     :param save_filename: the filename to save
     """
+    # 确保tmp文件夹存在
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
+
+    # 在tmp文件夹下保存文件
+    local_filename = os.path.join('tmp', save_filename)
+
     # 发送一个HTTP请求到URL
     response = requests.get(url, stream=True)
 
@@ -16,14 +24,14 @@ def download_file(url: str, save_filename: str):
     response.raise_for_status()
 
     # 打开文件以便写入
-    with open(save_filename, 'wb') as f:
+    with open(local_filename, 'wb') as f:
         for chunk in response.iter_content(chunk_size=8192):
             # 如果有chunk则写入文件
             if chunk:
                 f.write(chunk)
 
-    return save_filename
+    return os.path.abspath(local_filename)
 
-# print(download_file(
-#     'https://ask.qcloudimg.com/http-save/yehe-6781431/fc477e39f845d3a223e42da74bbf645a.png?imageView2/2/w/1200',
-#     'demo.jpg'))
+print(download_file(
+    'https://ask.qcloudimg.com/http-save/yehe-6781431/fc477e39f845d3a223e42da74bbf645a.png?imageView2/2/w/1200',
+    'demo.jpg'))
