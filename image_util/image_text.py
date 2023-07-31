b7276f24de92fbb7ca9ae64afe477efbfb4c417e
hamburger <hanjbde@163.com>
2023-08-01 01:49:51 +0800

    fix

--- image_util/image_text.py
+++ image_util/image_text.py
@@ -6,7 +6,7 @@ model_id = "stabilityai/stable-diffusion-2-1"
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-# pipe = pipe.to("cuda")
+pipe = pipe.to("cuda")
 
 
 @openai_func
@@ -18,3 +18,6 @@ def text_to_image(text: str):
     image = pipe(text).images[0]
     image.save(f"""/tmp/{text}.png""")
     return f"""/tmp/{text}.png"""
+
+
+# text_to_image("a phone of a dog")
