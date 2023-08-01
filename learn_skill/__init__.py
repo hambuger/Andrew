01c52184087515ef5a3aeb32b35a6fb742c61dc1
hamburger <HanJBde@163.com>
2023-08-01 13:47:21 +0800

    优化

--- learn_skill/__init__.py
+++ learn_skill/__init__.py
@@ -4,8 +4,11 @@ import os
 # 获取当前工作目录
 current_directory = os.getcwd()
 
-# 导入模块
-for filename in os.listdir(os.path.join(current_directory, "learn_skill")):
-    if filename.endswith(".py") and filename != "__init__.py":
-        module_name = "learn_skill." + filename[:-3]
-        module = importlib.import_module(module_name)
+try:
+    # 导入模块
+    for filename in os.listdir(os.path.join(current_directory, "learn_skill")):
+        if filename.endswith(".py") and filename != "__init__.py":
+            module_name = "learn_skill." + filename[:-3]
+            module = importlib.import_module(module_name)
+except Exception as e:
+    print(e)
